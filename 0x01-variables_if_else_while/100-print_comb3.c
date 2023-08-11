63643dbead3adb348e5a96a0d9e927097952e29d
HovsepJoe <hoteyinjoseph@gmail.com>
2023-08-11 09:55:59 -0700

    Fixed 3 tasks

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-
+/**
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int digit1, digit2;
@@ -13,8 +18,7 @@ int main(void)
 			putchar((digit2 % 10) + '0');
 
 			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
+				continue;			
 			putchar(',');
 			putchar(' ');
 		}
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-
+/**
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int digit1, digit2, digit3;
@@ -16,9 +21,8 @@ int main(void)
 				putchar((digit3 % 10) + '0');
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
-				putchar(',');
+					continue;	
+		         	putchar(',');
 				putchar(' ');
 			}
 		}

2adb5736e7c5f55f34e0abed41f6578450af0579
HovsepJoe <hoteyinjoseph@gmail.com>
2023-08-16 02:08:52 -0700

    fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,54 +12,22 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    if (year % 4 == 0)
-    {
-        if (year % 100 == 0)
-        {
-            if (year % 400 == 0)
-            {
-                if (month >= 2 && day >= 60)
-                {
-                    day++;
-                }
+    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int leap = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
 
-                printf("Day of the year: %d\n", day);
-                printf("Remaining days: %d\n", 366 - day);
-            }
-            else
-            {
-                if (month == 2 && day > 28)
-                {
-                    printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-                }
-                else
-                {
-                    printf("Day of the year: %d\n", day);
-                    printf("Remaining days: %d\n", 365 - day);
-                }
-            }
-        }
-        else
-        {
-            if (month >= 2 && day >= 60)
-            {
-                day++;
-            }
-
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 366 - day);
-        }
-    }
-    else
+    if (month < 1 || month > 12 || day < 1 || day > days_in_month[month] + (month == 2 && leap))
     {
-        if (month == 2 && day > 28)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
+        printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+        return;
     }
-}
+
+    int day_of_year = 0;
+    for (int i = 1; i < month; i++)
+    {
+        day_of_year += days_in_month[i] + (i == 2 && leap);
+    }
+    day_of_year += day;
+
+    printf("Day of the year: %d\n", day_of_year);
+    printf("Remaining days: %d\n", 365 + leap - day_of_year);
+} 

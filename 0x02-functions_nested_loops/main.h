a0d735752e42cb8e71218716a12abae092b80f89
HovsepJoe <hoteyinjoseph@gmail.com>
2023-08-15 22:56:39 -0700

    fxed

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,40 @@
 #define MAIN_H
 
 int _putchar(char c);
+
 int _putchar(char c);
 void print_alphabet_x10(void);
+
+int _putchar(char c);
+void print_alphabet(void);
+
+int _putchar(char c);
+int _islower(int c);
+void print_alphabet(void);
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+void print_alphabet(void);
+
+int _putchar(char c);
+int print_sign(int n);
+
+int _putchar(char c);
+int _abs(int n);
+
+int _putchar(char c);
+int print_last_digit(int n);
+
+int _putchar(char c);
+void jack_bauer(void);
+
+int _putchar(char c);
+void times_table(void);
+
+int _putchar(char c);
+int add(int num1, int num2);
+
+int _putchar(char c);
+void print_to_98(int n);
 #endif /* MAIN_H */

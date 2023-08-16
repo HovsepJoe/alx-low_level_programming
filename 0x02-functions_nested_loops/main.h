8f803c357adf665528ab067ba024c13f210ecc9f
HovsepJoe <hoteyinjoseph@gmail.com>
2023-08-16 00:03:42 -0700

    fixed

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,55 +2,20 @@
 #define MAIN_H
 
 int _putchar(char c);
-
-int _putchar(char c);
 void print_alphabet_x10(void);
-
-int _putchar(char c);
 void print_alphabet(void);
-
-int _putchar(char c);
-int _islower(int c);
-void print_alphabet(void);
-
-int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-void print_alphabet(void);
-
-int _putchar(char c);
-int _isalpha(int c);
-
-int _putchar(char c);
 int print_sign(int n);
-
-int _putchar(char c);
 int _abs(int n);
-
-int _putchar(char c);
 int print_last_digit(int n);
-
-int _putchar(char c);
 void jack_bauer(void);
-
-int _putchar(char c);
 void times_table(void);
-
-int _putchar(char c);
 int add(int num1, int num2);
-
-int _putchar(char c);
 void print_to_98(int n);
-
-int _putchar(char c);
 void print_times_table(int n);
-
-int _putchar(char c);
-int sum_of_multiples(int limit)
-
-int _putchar(char c);
+int sum_of_multiples(int limit);
 void print_fibonacci_sequence(int n);
-
-int _putchar(char c);
 int fibonacci_sum_even(int limit);
+
 #endif /* MAIN_H */

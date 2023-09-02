9748e25a1cf89039247d8b674168019d7aac3ad4
HovsepJoe <hoteyinjoseph@gmail.com>
2023-09-01 22:32:08 -0700

    done and errors fixed

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Write a program that prints all arguments it receives
- *        All arguments should be printed, including the first on
- * @argc: This is the argument count 
+ * All arguments should be printed, including the first on
+ * @argc: This is the argument count
  * @argv: This is the argument vector
  * Return: 0;
  */
